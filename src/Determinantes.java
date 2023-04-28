76e82623853341f1295a9c3aa6b46741fe7c7808
= <24mdavid25@gmail.com>
2023-04-28 17:48:13 -0600

    Se subio la actualizacion

---
+++ src/Determinantes.java
@@ -0,0 +1,4 @@
+public interface Determinantes{
+    void sacaX(double[][] matriz, double C1, double C2);
+    void sacaY(double matriz[][],double C1,double C2);
+}
--- src/GenerarMatriz.java
+++ src/GenerarMatriz.java
@@ -1,12 +1,9 @@
 import java.util.Scanner;
 
-public class GenerarMatriz{
+public class GenerarMatriz implements Determinantes{
 
     public void resolverEcuacion(){
-
         Scanner scanner =new Scanner(System.in);
-
-        SacarXY sacarXY = new SacarXY();
         double C1,C2;
 
         System.out.println("Ingrese los valores de C1 : ");
@@ -20,7 +17,37 @@ public class GenerarMatriz{
                 matriz[i][j] = scanner.nextDouble();
             }
         }
-        sacarXY.sacaX(matriz,C1,C2);
-        sacarXY.sacaY(matriz,C1,C2);
+        sacaX(matriz,C1,C2);
+        sacaY(matriz,C1,C2);
+    }
+    @Override
+    public void sacaX(double[][] matriz, double C1, double C2) {
+        double mult1,mult2,div,resta1,resta2,mult11,mult22;
+        mult1= C1 * matriz[1][1];
+        mult2= C2 * matriz[0][1];
+        resta1= mult1 -mult2;
+
+        mult11 = matriz[0][0]*matriz[1][1];
+        mult22 = matriz[1][0]*matriz[0][1];
+        resta2= mult11 -mult22;
+
+        div= resta1/resta2;
+        System.out.println("El resultado es X :"+div);
+    }
+
+    @Override
+    public void sacaY(double[][] matriz, double C1, double C2) {
+        double mult1,mult2,div,resta1,resta2,mult11,mult22;
+
+        mult1 = C2 * matriz[0][0];
+        mult2 = C1 * matriz[1][0];
+        resta1 = mult1 - mult2;
+
+        mult11 = matriz[0][0] * matriz[1][1];
+        mult22 = matriz[1][0] * matriz[0][1];
+        resta2=mult11 - mult22;
+
+        div =  resta1/resta2;
+        System.out.println("El resultado de Y :"+div);
     }
 }
